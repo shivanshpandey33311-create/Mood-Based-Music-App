c5ebc949d96fe11a82ec61466f2c0f8e2bf775fa
shivanshpandey33311-create <shivanshpandey33311@gmail.com>
2026-07-27 20:53:16 +0530

    Update deezer_api.py

--- deezer_api.py
+++ deezer_api.py
@@ -1,1 +1,28 @@
+import requests
 
+def get_songs(mood, limit=10):
+    url = "https://api.deezer.com/search"
+
+    params = {
+        "q": mood,
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200:
+        return []
+
+    data = response.json()
+
+    songs = []
+
+    for track in data["data"][:limit]:
+        songs.append({
+            "title": track["title"],
+            "artist": track["artist"]["name"],
+            "album": track["album"]["title"],
+            "cover": track["album"]["cover_medium"],
+            "preview": track["preview"],
+        })
+
+    return songs
